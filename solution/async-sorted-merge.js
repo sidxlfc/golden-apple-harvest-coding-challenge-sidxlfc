2e6f231ed014771ff56db956f987e42783445623
Siddharth Shah <sidshah2@duosecurity.com>
2025-04-14 23:45:47 -0400

    - some code cleanup

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -6,19 +6,15 @@ import MinHeap from "./min-heap.js";
 export const asyncSortedMerge = async(logSources, printer) => {
 
     const heap = new MinHeap();
-    let activeSources = logSources.length;
 
     // Producer: Fetch entries from log sources concurrently
     const fetchEntries = async (source, index) => {
         while (true) {
             const entry = await source.popAsync();
-            if (!entry) {
-                // Mark this source as drained
-                activeSources--;
-                break;
+            if (entry) {
+                // Insert the entry into the heap
+                heap.insert({ date: entry.date, msg: entry.msg, sourceIndex: index });   
             }
-            // Insert the entry into the heap
-            heap.insert({ date: entry.date, msg: entry.msg, sourceIndex: index });
         }
     };
 
@@ -26,11 +22,9 @@ export const asyncSortedMerge = async(logSources, printer) => {
     await Promise.all(logSources.map((source, index) => fetchEntries(source, index)));
 
     // Consumer: Process the heap until all sources are drained
-    while (activeSources > 0 || !heap.isEmpty()) {
-        if (!heap.isEmpty()) {
-            const { date, msg } = heap.remove();
-            printer.print({ date, msg });
-        }
+    while (!heap.isEmpty()) {
+        const { date, msg } = heap.remove();
+        printer.print({ date, msg });
     }
 
     printer.done(logSources, "Async sort complete.");
